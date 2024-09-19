feb7ec0076a14829cdff7b1a6d2691b34400ad1e
Dead4ever07 <luis2004007@gmail.com>
2024-09-19 08:59:54 +0100

    commit testing

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+    vector<string> array = {"09","27","34","12"};
+    cout<<((array[0] + array[1])+(array[2] + array[3]));
+}
