a27499616b3c713f6178bfa3a304e35ae97e0578
Dead4ever07 <luis2004007@gmail.com>
2024-09-19 11:08:03 +0100

    testing pointers in classes

--- test.cpp
+++ test.cpp
@@ -4,9 +4,34 @@
 #include <algorithm>
 using namespace std;
 
+class Test
+{
+    public:
+    Test(int* n1): n1_(n1){}
+    void print()
+    {
+        cout<<n1_<<' '<<n1_<<'\n';
+    }
+
+
+
+    private:
+    int* n1_;
+
+};
+
+
+
+
+
+
 
 int main()
 {
-    vector<string> array = {"09","27","34","12"};
-    cout<<((array[0] + array[1])+(array[2] + array[3]));
+    vector<int> v;
+    int n1 = 20;
+    v.push_back(n1);
+    cout<< &n1<<' '<<&v[0]<<'\n';
+    Test t(&n1);
+    t.print();
 }
