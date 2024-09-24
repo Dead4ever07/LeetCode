809154cbb04793f6d3a81affa7819fdaea584ffc
Dead4ever07 <luis2004007@gmail.com>
2024-09-24 21:27:09 +0100

    tentativa do 2

--- 2.cpp
+++ 2.cpp
@@ -1,6 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
+
+
+
+    ListNode* addRecursive(ListNode*l1, ListNode* l2, int pass )
+    {
+        if(l1 == nullptr && l2 == nullptr)
+        {
+            return nullptr;
+        }else if(l2 == nullptr)
+        {
+            ListNode result((l1->val + pass)%10, addRecursive(l1->next,nullptr,(l1->val+pass)/10));
+            ListNode* ptr = &result; 
+            
+            return ptr;
+        }else if(l1 == nullptr)
+        {
+            ListNode result((l2->val + pass)%10, addRecursive(nullptr,l2->next,(l2->val+pass)/10));
+            ListNode* ptr = &result; 
+            
+            return ptr;
+        }else
+        {
+            ListNode result((l1->val + l2->val + pass)%10, addRecursive(l1->next,l2->next,(l1->val + l2->val)/10));
+            ListNode* ptr = &result; 
+            
+            return ptr;
+        }
+ 
+    
+    }
+
+
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        return addRecursive(l1, l2, 0);
         
     }
 };
