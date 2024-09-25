b7f246612c6881b9b11439f1407e05c89d987f94
Dead4ever07 <luis2004007@gmail.com>
2024-09-25 15:11:12 +0100

    Revert "tentativa de 2.cpp"
    This reverts commit fd67b4b19533945c2981468568704bb059025abf.

--- 2.cpp
+++ 2.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
-
-struct ListNode {
-      int val;
-      ListNode *next;
-      ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
-      ListNode(int x, ListNode *next) : val(x), next(next) {}
- };
- 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
 
@@ -48,9 +48,3 @@ public:
         
     }
 };
-
-
-int main()
-{
-    ListNode
-}
