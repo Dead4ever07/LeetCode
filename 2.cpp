fd67b4b19533945c2981468568704bb059025abf
Dead4ever07 <luis2004007@gmail.com>
2024-09-25 14:58:12 +0100

    tentativa de 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,13 +1,13 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+#include <iostream>
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
 class Solution {
 public:
 
@@ -48,3 +48,9 @@ public:
         
     }
 };
+
+
+int main()
+{
+    ListNode
+}
